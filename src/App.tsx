4cb22cfc38ea918f4f04b9d275e8ca833192a9a0
gardanier22 <agardanier22@gmail.com>
2024-03-08 15:19:15 -0700

    made all the changes to the app.tsx file in one fell swoop and made it look how the requirements asked for

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teams from './CollegeBasketballTeams.json';
+
+const teamsList = teams.teams;
+
+interface NcaamTeams {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+class Team extends React.Component<NcaamTeams> {
+  render() {
+    const team = this.props;
+    return (
+      <div>
+        <h3>School: {team.school}</h3>
+        <h4>Mascot: {team.name}</h4>
+        <h4>
+          Location: {team.city}, {team.state}
+        </h4>
+      </div>
+    );
+  }
+}
+
+function Teams() {
+  return (
+    <div>
+      {teamsList.map((team) => (
+        <Team {...team} />
+      ))}
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>Lets Go Dancing!!!</h1>
+      <h2>
+        Below we have all the information about the teams competeing at the ball
+      </h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Teams />
     </div>
   );
 }
